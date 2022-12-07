cfc522364f94f54a84d2e1aa6c1c4998466842f3
Alex Shaw <alex@theshaws.net>
2022-12-07 22:22:28 +0000

    added google tags and badly formatted footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,6 +24,26 @@ class Template extends React.Component {
     return (
       <>
         <Seo />
+        <Script id="clarity">
+        {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "eulfh7sro3");
+          `}
+        </Script>
+        
+<Script id="gtag_l" src="https://www.googletagmanager.com/gtag/js?id=G-CWS43VLQ2P"></Script>
+<Script id="gtag">
+  {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-CWS43VLQ2P');
+  `}
+</Script>
         <Navigation />
         <main>{children}</main>
         <Footer />
